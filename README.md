10912ca4c624e7b94b66a1b413c7e948484fb00f
Will Robertson <aliask@gmail.com>
2021-10-30 23:52:45 +1100

    Pictures

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
-<img src="ledmatrix.png" align="left" width="50px" height="50px">
+<img src="img/ledmatrix.png" align="left" width="50px" height="50px">
 
 # LED Matrix Server
 
 LED Matrix Server runs on a Raspberry Pi and passes incoming streaming video data to a connected WS2812 LED Matrix.
 
-![LED Matrix Context Diagram](diagram.drawio.png)
+![LED Matrix Context Diagram](img/diagram.drawio.png)
 
-## Hardware Compatibility
+[![Shiny lights](img/ledmatrix.gif)](img/ledmatrix.mp4)
+
+## Hardware
+
+### Compatibility
 
 This utility should perform adequately on the following devices:
 - Raspberry Pi 3A+
@@ -16,27 +20,44 @@ This utility should perform adequately on the following devices:
 
 Earlier devices may also work but may run into performance issues, especially with higher framerates.
 
+### LED Matrix Module
+
+I used two [16x16 modules from AliExpress](https://www.aliexpress.com/item/1005002320547245.html) wired in series for a display size of 32x16 pixels.
+
+In theory, all of these LEDs could draw more than 10A (1500 individual LEDs), but after connecting to my bench power supply I didn't see anything higher than a couple of amps, even at full brightness.
+
+I decided to be lazy and run the matrix directly from the Raspberry Pi power line, which means making sure your USB power supply is a good brand. It needs to feed the Pi and all these LEDs.
+
+![Wiring Diagram](img/wiring.drawio.png)
+
 ## Installation
 
 This project has been Dockerised to assist with dependency management and portability. 
 
 Follow these steps on the Raspberry Pi to get this working.
 
-0. Ensure Docker and `docker-compose` are [installed](https://www.docker.com/blog/happy-pi-day-docker-raspberry-pi/) and [working](https://github.com/docker/for-linux/issues/1196)
+0. Ensure `docker` and `docker-compose` are [installed](https://www.docker.com/blog/happy-pi-day-docker-raspberry-pi/) and [working](https://github.com/docker/for-linux/issues/1196)
 
-1. Clone this repo
+1. Clone this repo 🐑
 
 `git clone git@github.com:aliask/ledmatrix.git`
 
-2. Build the docker image
+2. Build the Docker image 🐋
 
 `docker-compose build`
 
 This will take a while, depending on which model you have.
 
-3. Run it!
+3. Run it! 👟
 
 `docker-compose up -d`
+
+## Photos
+
+<img src="img/ledmatrix-rear.jpg" width="450px"> <br>
+
+<img src="img/ledmatrix-side.jpg" width="450px">
+
 
 ## Related Projects
 
--- main.py
+++ main.py
@@ -13,23 +13,27 @@ from rpi_ws281x import Color
 
 from ledmatrix import *
 
+
 def grouper(n, iterable, fillvalue=None):
     "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
     args = [iter(iterable)] * n
     return zip_longest(fillvalue=fillvalue, *args)
 
+
 class LEDServer:
 
-    UDP_PORT = int(os.environ.get('LEDSERVER_PORT', 20304))
+    UDP_PORT = int(os.environ.get("LEDSERVER_PORT", 20304))
     DATA_TIMEOUT_SEC = 2
     is_receiving = True
 
     def __reset_timer(self):
-        self.nodata_timer = datetime.datetime.now() + datetime.timedelta(seconds = self.DATA_TIMEOUT_SEC)
+        self.nodata_timer = datetime.datetime.now() + datetime.timedelta(
+            seconds=self.DATA_TIMEOUT_SEC
+        )
 
     def __has_data_stopped(self):
         timedout = self.nodata_timer < datetime.datetime.now()
-        if(timedout and self.is_receiving):
+        if timedout and self.is_receiving:
             self.is_receiving = False
             return True
         else:
@@ -45,7 +49,11 @@ class LEDServer:
         self.__graceful_exit()
 
     def __init__(self):
-        logging.basicConfig(level = logging.INFO)
+        logging.basicConfig(
+            format="%(asctime)s [%(levelname)-8s] %(message)s",
+            level=logging.INFO,
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
         self.leds = LEDMatrix()
         self.server = UDPServer(self.UDP_PORT, self.DATA_TIMEOUT_SEC)
         self.__reset_timer()
@@ -59,7 +67,10 @@ class LEDServer:
 
         try:
             self.server.start()
-            logging.info("Started listening for LED Matrix data on udp://%s:%d" % (socket.gethostname(), self.UDP_PORT))
+            logging.info(
+                "Started listening for LED Matrix data on udp://%s:%d"
+                % (socket.gethostname(), self.UDP_PORT)
+            )
         except:
             logging.error("Could not open UDP socket")
             quit(1)
@@ -68,14 +79,14 @@ class LEDServer:
         signal.signal(signal.SIGINT, self.handle_signal)
 
         try:
-            image = self.leds.loadImage('/app/pattern.png')
+            image = self.leds.loadImage("/app/pattern.png")
             self.leds.displayFrame(image)
         except:
             logging.warning("Couldn't load image")
 
         while True:
             try:
-                if(self.__has_data_stopped()):
+                if self.__has_data_stopped():
                     logging.info("Stopped receiving data - putting display to sleep")
                     self.leds.clearScreen()
 
@@ -84,22 +95,24 @@ class LEDServer:
 
                 logging.debug("Received packet")
 
-                if(self.is_receiving is False):
+                if self.is_receiving is False:
                     logging.info("Receiving data from udp://%s:%s" % packet["addr"])
                     self.is_receiving = True
 
-                if(packet['type'] == "frame"):
+                if packet["type"] == "frame":
                     # Take received packet and format for LED panel
-                    (height,width,pixels) = packet["frame"]
-                    frame = LedFrame(height,width)
-                    for pixel in grouper(4, list(struct.unpack('B'*frame_pixels*4, pixels))):
-                        (r,g,b,a) = pixel
-                        frame.pixels.append(Color(r,g,b))
+                    (height, width, pixels) = packet["frame"]
+                    frame = LedFrame(height, width)
+                    for pixel in grouper(
+                        4, list(struct.unpack("B" * frame_pixels * 4, pixels))
+                    ):
+                        (r, g, b, a) = pixel
+                        frame.pixels.append(Color(r, g, b))
 
                     self.leds.displayFrame(frame)
-                elif(packet['type'] == "command" and packet['command'] == "brightness"):
-                    logging.info("Setting brightness to %d" % packet['value'])
-                    self.leds.setBrightness(packet['value'])
+                elif packet["type"] == "command" and packet["command"] == "brightness":
+                    logging.info("Setting brightness to %d" % packet["value"])
+                    self.leds.setBrightness(packet["value"])
 
                 self.__reset_timer()
 
@@ -109,6 +122,6 @@ class LEDServer:
                 logging.error("Error while processing: %s" % e)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = LEDServer()
     app.run()
