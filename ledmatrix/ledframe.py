802f3704b050225aabfdd443be14d0693c563c77
Will Robertson <aliask@gmail.com>
2022-08-14 11:41:17 +1000

    WIP: More dataclasses

--- ledmatrix/ledframe.py
+++ ledmatrix/ledframe.py
@@ -1,9 +1,26 @@
 # LedFrame dataclass
 
 from dataclasses import dataclass, field
+from itertools import zip_longest
+import struct
+
+from rpi_ws281x import Color
+
+def grouper(n, iterable, fillvalue=None):
+    "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return zip_longest(fillvalue=fillvalue, *args)
+
 
 @dataclass
 class LedFrame:
   height: int
   width: int
   pixels: list = field(default_factory=list)
+
+  def fill_from_bytearray(self, pixeldata: bytearray):
+    self.pixels = []
+    pixel_list = list(struct.unpack("B" * self.height * self.width * 4, pixeldata))
+    for pixel in grouper(4, pixel_list):
+      (r, g, b, a) = pixel
+      self.pixels.append(Color(r, g, b))
--- ledmatrix/udpserver.py
+++ ledmatrix/udpserver.py
@@ -3,6 +3,8 @@
 
 import logging, socket, struct, select
 from enum import Enum
+from dataclasses import dataclass, field
+from typing import Any
 
 class FrameException(Exception):
     pass
@@ -13,31 +15,68 @@ class NoDataException(Exception):
 class Commands(Enum):
     SetBrightness = 0
 
+@dataclass
+class NetworkFrame:
+    source: Any = field(default=None, init=False)
+
+@dataclass
+class CommandFrame(NetworkFrame):
+    IDENT = 0x4321
+
+    command: Commands
+    value: int
+
+@dataclass
+class ImageFrame(NetworkFrame):
+    IDENT = 0x1234
+
+    pixels: bytearray
+    height: int
+    width: int
+
 class UDPServer:
 
     ALL_IFACES = "0.0.0.0"
-    FRAME_IDENT = 0x1234
-    COMMAND_IDENT = 0x4321
     FRAME_HEADER_SIZE = 8
     FRAME_PIXEL_SIZE = 4
 
-    def __init__(self, port, timeout):
+    def __init__(self, port: int, timeout: int) -> None:
         self.port = port
         self.timeout = timeout
 
-    def start(self):
+    def start(self) -> None:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((self.ALL_IFACES, self.port))
         self.sock.settimeout(self.timeout)
 
-    def __empty_socket(self):
+    def __empty_socket(self) -> None:
         input = [self.sock]
         while 1:
             inputready, o, e = select.select(input,[],[], 0.0)
             if len(inputready)==0: break
             for s in inputready: s.recv(1)
 
-    def getFrame(self, num_pixels):
+    def __parse_command(self, data) -> CommandFrame:
+        (command, value) = struct.unpack("BB", data)
+
+        if(command == Commands.SetBrightness.value):
+            return CommandFrame(command=Commands.SetBrightness, value=value)
+        else:
+            raise FrameException("Unknown command received: 0x%x" % command)
+
+    def __parse_image(self, data, num_pixels) -> ImageFrame:
+        (height, width, length) = struct.unpack("HHH", data[0:6])
+        pixels = bytearray(data[6:])
+
+        if(length != len(pixels)):
+            raise FrameException("Header says %i pixel bytes, but got %i" % (length, len(pixels)))
+
+        if(length != num_pixels * self.FRAME_PIXEL_SIZE):
+            raise FrameException("Not enough data in the packet to make a frame")
+
+        return ImageFrame(height=height, width=width, pixels=pixels)
+
+    def get_frame(self, num_pixels: int) -> NetworkFrame:
         num_bytes = num_pixels * self.FRAME_PIXEL_SIZE + self.FRAME_HEADER_SIZE
         logging.debug("Emptying socking before RX")
         self.__empty_socket()
@@ -50,38 +89,16 @@ class UDPServer:
             except socket.gaierror:
                 logging.warn("Error during hostname lookup for %s" % addr[0])
 
-            # Command packet
-            if(len(data) == 4):
-                (ident, command, value) = struct.unpack("HBB", data)
+            frame_type = struct.unpack("H", data[0:2])[0]
+            if(frame_type == CommandFrame.IDENT):
+                parsed = self.__parse_command(data[2:])
+            elif(frame_type == ImageFrame.IDENT):
+                parsed = self.__parse_image(data[2:], num_pixels)
+            else:
+                raise FrameException("Unknown frame identity: 0x%x" % frame_type)
 
-                if(ident != self.COMMAND_IDENT):
-                    raise FrameException("Wrong command ident received (got 0x%x)" % ident)
+            parsed.source = addr
+            return parsed
 
-                if(command == Commands.SetBrightness.value):
-                    packet = {
-                        "type": "command",
-                        "addr": addr,
-                        "command": "brightness",
-                        "value": value
-                    }
-                    return packet
-                else:
-                    raise FrameException("Unknown command received: 0x%x" % command)
-
-            # Frame packet
-            (ident, height, width, length) = struct.unpack("HHHH", data[0:8])
-            if(ident != self.FRAME_IDENT):
-                raise FrameException("Wrong frame ident received (got 0x%x)" % ident)
-            pixels = bytearray(data[8:])
-            if(length != len(pixels)):
-                raise FrameException("Header says %i pixel bytes, but got %i" % (length, len(pixels)))
-            if(length != num_pixels * self.FRAME_PIXEL_SIZE):
-                raise FrameException("Not enough data in the packet to make a frame")
-            packet = {
-                "type": "frame",
-                "addr": addr, 
-                "frame": (height, width, pixels)
-            }
-            return packet
         except socket.timeout:
             raise NoDataException(socket.timeout)
